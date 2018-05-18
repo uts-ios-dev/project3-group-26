183d88c9088d4bb5023b5099205d3bad9ad2288b
andy10R <andychenzt@gmail.com>
2018-05-18 15:23:00 +1000

    add speech function

---
+++ project3-group-26/Speech.swift
@@ -0,0 +1,12 @@
+//
+//  Speech.swift
+//  project3-group-26
+//
+//  Created by Zhongtao  Chen on 18/5/18.
+//  Copyright © 2018 group-26. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+
