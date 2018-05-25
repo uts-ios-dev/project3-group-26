15058bb94e3334747e215fb3c0279d385ceb47e7
andy10R <andychenzt@gmail.com>
2018-05-25 16:34:19 +1000

    Correct bugs and clean up
    fix the bugs in decide course
    and clean up the code.

--- project3-group-26/MapViewController.swift
+++ project3-group-26/MapViewController.swift
@@ -85,7 +85,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         
         // get nearby places, save in array,
         fetchLocationInfo(parameters: searchParams)
-        searchPlace(placeID: "1")
     }
     
     func convetToAddress(location: CLLocation) {
@@ -124,7 +123,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
                     self.nearbyPlaces = jsonResult.results
                     print(self.nearbyPlaces)
                     
-                    self.decideDirection(nearbyPlaces: self.nearbyPlaces)
+                    self.decideCourse(nearbyPlaces: self.nearbyPlaces)
                 } catch {
                     print("error2: \(error)")
                 }
@@ -160,63 +159,18 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         })
     }
     
-    func getCoordnate(placeID: String) -> CLLocationCoordinate2D {
-        let placeID = "ChIJH7WPyCeuEmsRaZs_uxf4eXU"
-        
-        placesClient = GMSPlacesClient.shared()
-        var thisPlaceCoor: CLLocationCoordinate2D!
-        placesClient.lookUpPlaceID(placeID, callback: { (place, error) -> Void in
-            if let error = error {
-                print("lookup place id query error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let place = place else {
-                print("No place details for \(placeID)")
-                return
-            }
-            
-            // !!! place.coordinate.latitude / longtitude to get the location of the place if the user want to go there following the navigation  !!!!!!
-            print("Place name \(place.name)")
-            print("Place address \(place.formattedAddress!)")
-            print("Place placeID \(place.placeID)")
-            print("Place attributions \(place.coordinate)")
-            thisPlaceCoor = place.coordinate
-        })
-        return thisPlaceCoor!
-    }
-    
-    func decideDirection(nearbyPlaces: [NearbyPlace]) {
-        print("decide Direction")
-        print(nearbyPlaces)
-        let currentLocation = CLLocationCoordinate2DMake(-33.8841070, 151.2003266)
-        for place in nearbyPlaces {
-            let placeCoordinate = getCoordnate(placeID: place.place_id)
-            decideFrontBack(currentCoord: currentLocation, nearbyPlaceCoord: placeCoordinate)
-        }
-        
-    }
-    
-    func decideFrontBack(currentCoord: CLLocationCoordinate2D, nearbyPlaceCoord: CLLocationCoordinate2D) {
-        let vector1: (Double, Double) = (-1, 1)
-        let vector2: (Double, Double)  = (nearbyPlaceCoord.latitude - currentCoord.latitude, nearbyPlaceCoord.longitude - currentCoord.longitude)
-        let multi = vector1.0 * vector2.0 + vector1.1 * vector2.1
-        if multi > 0 {
-            print("front!!")
-        }
-    }
-    
     // refer to coordinate verdict course of the nearby place
     // still has bug!!!
     func decideCourse(nearbyPlaces: [NearbyPlace]) {
         print("decide Course")
         print(nearbyPlaces)
-        let currentLocationCoordinate = CLLocationCoordinate2DMake(-33.8841070, 151.2003266)
+//        let currentLocationCoordinate = CLLocationCoordinate2DMake(-33.8841070, 151.2003266)          // for test
+        guard let _ = currentLocation else { return }
+        let currentLocationCoordinate = currentLocation!.coordinate
         for place in nearbyPlaces {
             let placeCoordinate = CLLocationCoordinate2DMake(place.geometry.location.lat, place.geometry.location.lng)
             print(place.name)
             print("get coord: \(placeCoordinate)")
-            //            decideFrontBack(currentCoord: currentLocation, nearbyPlaceCoord: placeCoordinate)
             let bearing = getBearing(currentCoord: currentLocationCoordinate, nearbyPlaceCoord: placeCoordinate)
             print("bearing is: \(bearing)")
             
@@ -241,32 +195,21 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     func getBearing(currentCoord: CLLocationCoordinate2D, nearbyPlaceCoord: CLLocationCoordinate2D) -> Double {
         let vector1: (Double, Double) = (currentCoord.latitude, currentCoord.longitude)
         let vector2: (Double, Double)  = (nearbyPlaceCoord.latitude, nearbyPlaceCoord.longitude)
-        let x = vector2.0 - vector1.0
-        let y = vector2.1 - vector1.1
-        let delta = atan2(abs(x), abs(y))
+        let y = vector2.0 - vector1.0
+        let x = vector2.1 - vector1.1
+        let delta = atan2(abs(x), abs(y)) * 180 / .pi
         print("delta: \(delta)")
-        let headingBearing: CLLocationDirection  = 90
+        let headingBearing: CLLocationDirection  = 0
         var northPlaceBearing: Double!
-        //        if x > 0 && y > 0 {
-        //            northPlaceBearing = delta
-        //        }
-        //        else  if x > 0 && y < 0 {
-        //            northPlaceBearing = 180 - delta
-        //        } else if x < 0 && y < 0 {
-        //            northPlaceBearing = 180 + delta
-        //        } else if x < 0 && y > 0 {
-        //            northPlaceBearing = 360 - delta
-        //        } else if x == 0 && y > 0 {
-        //            return 360 - headingBearing
-        //        }
+        
         if x > Double(0) && y < Double(0) {
             northPlaceBearing = delta
         } else  if x > Double(0) && y > Double(0) {
             northPlaceBearing = Double(180) - delta
         } else if x < Double(0) && y > Double(0) {
-            northPlaceBearing = Double(180) + delta
-        } else if x < Double(0) && y < Double(0) {
             northPlaceBearing = Double(360) - Double(delta)
+        } else if x < Double(0) && y < Double(0) {
+            northPlaceBearing = Double(180) + delta
         }
             
         else if x == Double(0) && y > Double(0) {
