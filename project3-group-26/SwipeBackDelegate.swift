c0a90cd9482bf50d01680b614e56b5903cf658a4
ch3n0l1 <ch3n0l1@gmail.com>
2018-05-28 21:57:49 +1000

    Add delegate
    Providing right swipe to back to last VC via the extensio of UIViewController, and use delegate to ensure unwind segue is initilized.

---
+++ project3-group-26/SwipeBackDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SwipeBackDelegate.swift
+//  project3-group-26
+//
+//  Created by Linli Chen on 28/5/18.
+//  Copyright © 2018 group-26. All rights reserved.
+//
+
+import Foundation
+
+protocol SwipeBackDelegate {
+    
+    var swipeBackSegue: String { get set }
+    
+    
+}
