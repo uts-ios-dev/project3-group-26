8049aea1e4deaa6aae501fdb26ce88099c93bde2
ch3n0l1 <ch3n0l1@gmail.com>
2018-06-02 18:22:17 +1000

    Change speech template
    Change the ways of getting speech template
    1. remove previously plist & plist related api
    2. add struct providing static props

--- project3-group-26/SpeechUtil.swift
+++ project3-group-26/SpeechUtil.swift
@@ -23,7 +23,6 @@ class SpeechUtil {
         var tmp = template
         for text in texts {
             tmp = tmp.replaceFirstOccurrence(of: placeHolder, with: text)
-            print(tmp)
         }
         return tmp
     }
@@ -37,3 +36,16 @@ extension String {
     }
 }
 
+struct SpeechTemplate {
+    
+    static let PAGE_INFO = "This page is %@ page, you can %@."
+    static let PAGE_INFO_SIMPLE = "This page is %@ page."
+    
+    static let BUTTON_INFO = "The page is divided into %@, which are %@ button respectively. You can %@."
+    static let GESTURE_INFO = "Single tap to read items' description, double tap to get more details."
+    static let BACK_GESTURE_INFO = "Right swpie to back to last page."
+    static let REPEAT_GESTURE_INFO = "Down swipe to listen page introduction again."
+    static let SPOT_INFO = "This direction is %@ whose top %@ spots are %@."
+    
+    
+}
